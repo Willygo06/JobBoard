ee217cd9a1ee2c86aa9389be770eb01ca1afaa4d
Clément Lores <clement.lores@epitech.eu>
2024-10-10 12:21:12 +0200

    Committing header and 'En savoir plus' function

--- frontend/src/backtotopbutton.js
+++ frontend/src/backtotopbutton.js
@@ -1,19 +1,42 @@
-// BackToTopButton.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const BackToTopButton = () => {
-  const handleScrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+const BackToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Ajouter un écouteur d'événement pour détecter le scroll
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 100) {  // Afficher le bouton après avoir défilé de 300px
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  // Fonction pour revenir en haut de la page
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Défilement en douceur
+    });
   };
 
   return (
     <button
-      onClick={handleScrollToTop}
-      className="fixed bottom-4 right-4 bg-blue-500 text-white rounded-full w-6 h-8 flex items-center justify-center shadow-lg hover:bg-blue-600 transition duration-300"
-      >
-        <i className="fa-solid fa-arrow-up"></i> {/* Add the className to the icon */}
-      </button>
+      className={`fixed bottom-4 right-4 bg-blue-500 text-white rounded-full p-2 shadow-lg transition-opacity duration-400 hover:bg-blue-600 ${isVisible ? 'opacity-100' : 'opacity-0'}`}
+      onClick={scrollToTop}
+      style={{ pointerEvents: isVisible ? 'auto' : 'none' }} // Désactiver le clic quand invisible
+    >
+      <i className="text-xl fa-solid fa-arrow-up"></i>
+    </button>
   );
 };
 
-export default BackToTopButton;
+export default BackToTop;
