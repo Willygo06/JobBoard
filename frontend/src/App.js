2fb43835808a1c8b75d40e6308bb4f37b8293570
Clément Lores <clement.lores@epitech.eu>
2024-10-10 11:06:56 +0200

    Added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './header';
 import JobBoard from './jobboard';
 import Footer from './footer';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      <Header />
       <div className="flex-grow">
       <JobBoard />
       </div>
--- frontend/src/footer.js
+++ frontend/src/footer.js
@@ -3,24 +3,24 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-gray-800 text-white py-6">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <footer className="w-full bg-gray-800 text-white py-4">
+      <div className="max-w-7xl mx-auto px-4 py-1 sm:px-6 lg:px-8">
         <div className="flex flex-col">
-          <div className="mb-4 sm:mb-6">
+          <div className="mb-4 sm:mb-2">
             <h1 className="text-2xl font-bold">LFG</h1>
             <p className="text-gray-400 mt-2">&copy; 2024 LFG. Tous droits réservés.</p>
           </div>
           <div>
             <nav className="space-x-6">
-              <a href="#about" className="hover:text-blue-500 transition duration-200">À propos</a>
-              <a href="#jobs" className="hover:text-blue-500 transition duration-200">Offres d'emploi</a>
+              <a href="#about" className="hover:text-blue-500 transition duration-200 whitespace-pre-wrap">À propos</a>
+              <a href="#jobs" className="hover:text-blue-500 transition duration-200 whitespace-pre-wrap">Offres d'emploi</a>
               <a href="#contact" className="hover:text-blue-500 transition duration-200">Contact</a>
             </nav>
           </div>
         </div>
-        <div className="mt-6 text-center text-gray-400">
+        <div className="mt-3 text-center text-gray-400">
           <p>Suivez-nous sur:</p>
-          <div className="flex justify-center space-x-4 mt-2">
+          <div className="flex justify-center space-x-4 mt-1">
             <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-facebook fa-lg hover:text-blue-500 transition duration-200"></i>
             </a>
